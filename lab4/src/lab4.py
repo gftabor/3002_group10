097489e650ab2e01a82088432cc130a42e2771bb
Brandon <bvcoll@wpi.edu>
2016-11-28 16:03:29 -0500

    Added psudocode/some working code for obstacleExpansion

--- lab4/src/lab4.py
+++ lab4/src/lab4.py
@@ -19,6 +19,40 @@ def path_callback(input_path):
 	pointpub.publish(path[0])
 	index = 1
 	print"new point"
+
+#Expands obstacles in the grid to allow easier navagation.
+def obstacleExpansion(grid):
+
+	#TODO put all cells in grid in a global array.
+	cells = grid.data
+
+	#This array is used to designate cells that will become obstacles.
+	toBeObstacle = []
+
+	#Runs through all cells in the grid.
+	for i in cells:
+		#If a cell is not an obstacle and is too close to an obstacle add it to the array that will make designated cells obstacles.
+		if (cells[i] < 20) and isCloseToObstacle(cells[i], 0):
+			toBeObstacle.append(cells[i])
+
+	#Makes cells in toBeObstacle obstacles.
+	for j in toBeObstacle:
+		toBeObstacle.cells[j].isObstacle = True
+
+#Recursive function returns true if the cell passed is an obstacle, false if far away cells are not obstacles.
+def isCloseToObstacle(cell, startingLoc, distFromStart):
+	#BASE1: If cell reached is an obstacle return true.
+	if(cell.isObstacle):
+		return True
+	#BASE2: If distance from original cell is too long return false.
+	elif(distFromStart>3):
+		return False
+	#Check all adjacent cells if no base case is met.
+	else: #TODO Math to get cell locations of top and bottom cells.
+		return isCloseToObstacle(cells[startingLoc-1],startingLoc-1,distFromStart+1) and isCloseToObstacle(cells[startingLoc+1],startingLoc+1,distFromStart+1) and isCloseToObstacle() and isCloseToObstacle()
+
+
+
 def waypoint_callback():
 	global pointpub
 	global path
@@ -37,5 +71,10 @@ def run():
 
 
 if __name__ == '__main__':
+
+	global mapGrid
+
+
+
 	run()
     
